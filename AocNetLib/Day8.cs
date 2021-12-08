9aff124a9b08af6f1e826d94b03bb40b76b3de10
rsz166 <szabolcs.roman6@gmail.com>
2021-12-08 22:16:49 +0100

    day8

---
+++ AocNetLib/Day8.cs
@@ -0,0 +1,101 @@
+﻿namespace AocNetLib
+{
+    public class Day8
+    {
+        public string Solve(string input)
+        {
+            var x = ParseInput(input);
+            return x.Sum(x => x.Count(y => y == 1 || y == 4 || y == 7 || y == 8)).ToString();
+        }
+
+        private static List<int[]> ParseInput(string input)
+        {
+            var lines = input.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+            var decodedLines = lines.Select(x => DecodeLine(x)).ToList();
+            return decodedLines;
+        }
+
+        private static int[] DecodeLine(string x)
+        {
+            var parts = x.Split('|');
+            var key = parts[0].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var code = parts[1].Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            var decoder = new Decoder(key);
+            var result = decoder.Decode(code);
+            return result;
+        }
+
+        internal class Decoder
+        {
+            Dictionary<string, int> translation;
+            public Decoder(string[] key)
+            {
+                translation = new Dictionary<string, int>();
+                char[] segCF = key.First(x => x.Length == 2).ToCharArray();
+                char[] segACF = key.First(x => x.Length == 3).ToCharArray();
+                char[] segBCDF = key.First(x => x.Length == 4).ToCharArray();
+                char segA = segACF.First(x => !segCF.Contains(x));
+                char[][] d235 = key.Where(x => x.Length == 5).Select(x => x.ToCharArray()).ToArray();
+                char[] allOptions = "abcdefg".ToCharArray();
+                char[] segBE = allOptions.Where(x => d235.Count(y => y.Contains(x)) == 1).ToArray();
+                char segE = segBE.First(x => !segBCDF.Contains(x));
+                char segB = segBE.First(x => x != segE);
+                char[] segBD = segBCDF.Where(x => !segCF.Contains(x)).ToArray();
+                char segD = segBD.First(x => x != segB);
+                char[][] d069 = key.Where(x => x.Length == 6).Select(x => x.ToCharArray()).ToArray();
+                char[] segCDE = allOptions.Where(x => d069.Count(y => y.Contains(x)) == 2).ToArray();
+                char segC = segCDE.First(x => x != segD && x != segE);
+                char segF = segCF.First(x => x != segC);
+                char segG = allOptions.First(x => x != segA && x != segB && x != segC && x != segD && x != segE && x != segF);
+                translation[GenerateKey(segA, segB, segC, segE, segF, segG)] = 0;
+                translation[GenerateKey(segC, segF)] = 1;
+                translation[GenerateKey(segA, segC, segD, segE, segG)] = 2;
+                translation[GenerateKey(segA, segC, segD, segF, segG)] = 3;
+                translation[GenerateKey(segB, segC, segD, segF)] = 4;
+                translation[GenerateKey(segA, segB, segD, segF, segG)] = 5;
+                translation[GenerateKey(segA, segB, segD, segE, segF, segG)] = 6;
+                translation[GenerateKey(segA, segC, segF)] = 7;
+                translation[GenerateKey(segA, segB, segC, segD, segE, segF, segG)] = 8;
+                translation[GenerateKey(segA, segB, segC, segD, segF, segG)] = 9;
+            }
+
+            private static string GenerateKey(params char[] segments)
+            {
+                return string.Concat(segments.OrderBy(x => x));
+            }
+
+            internal int[] Decode(string[] code)
+            {
+                int[] ret = new int[code.Length];
+                for (int i = 0; i < code.Length; i++)
+                {
+                    ret[i] = Decode(code[i]);
+                }
+                return ret;
+            }
+
+            private int Decode(string v)
+            {
+                v = GenerateKey(v.ToCharArray());
+                return translation[v];
+            }
+        }
+
+        public string Solve2(string input)
+        {
+            var x = ParseInput(input);
+            return x.Sum(x => MargeDigits(x)).ToString();
+        }
+
+        private int MargeDigits(int[] x)
+        {
+            int num = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                num *= 10;
+                num += x[i];
+            }
+            return num;
+        }
+    }
+}
---
+++ AocNetTest/Day8Test.cs
@@ -0,0 +1,89 @@
+﻿using AocNetLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AocNetTest
+{
+    [TestClass]
+    public class Day8Test
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            string input = @"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce";
+            var solver = new Day8();
+            string output = solver.Solve(input);
+            Assert.AreEqual("26", output);
+        }
+
+        [TestMethod]
+        public void Test1_2()
+        {
+            string input = @"abcefg cf acdeg acdfg bcdf abdfg abdefg acf abcdefg abcdfg | cf";
+            var solver = new Day8();
+            string output = solver.Solve(input);
+            Assert.AreEqual("1", output);
+        }
+
+        [TestMethod]
+        public void Test1_3()
+        {
+            string input = @"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | be";
+            var solver = new Day8();
+            string output = solver.Solve(input);
+            Assert.AreEqual("1", output);
+        }
+
+        [TestMethod]
+        public void Final1()
+        {
+            string input = File.ReadAllText("../../../files/day8.txt");
+            var solver = new Day8();
+            string output = solver.Solve(input);
+            Console.WriteLine(output);
+            Assert.AreEqual("301", output);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            string input = @"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce";
+            var solver = new Day8();
+            string output = solver.Solve2(input);
+            Assert.AreEqual("61229", output);
+        }
+
+        [TestMethod]
+        public void Final2()
+        {
+            string input = File.ReadAllText("../../../files/day8.txt");
+            var solver = new Day8();
+            string output = solver.Solve2(input);
+            Console.WriteLine(output);
+            Assert.AreEqual("908067", output);
+        }
+    }
+}
