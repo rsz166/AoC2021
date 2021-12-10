a927a314a77abb2e82cf098026795b3ef57d3db1
rsz166 <szabolcs.roman6@gmail.com>
2021-12-10 19:32:18 +0100

    day10

---
+++ AocNetLib/Day10.cs
@@ -0,0 +1,112 @@
+﻿namespace AocNetLib
+{
+    public class Day10
+    {
+        public string Solve(string input)
+        {
+            var file = ParseInput(input);
+            return GetCorruptedScore(file).ToString();
+        }
+
+        private int GetCorruptedScore(string[] file)
+        {
+            return file.Sum(GetCorruptedScore);
+        }
+
+        private int GetCorruptedScore(string x)
+        {
+            Stack<char> opened = new Stack<char>();
+            for (int i = 0; i < x.Length; i++)
+            {
+                char c = x[i];
+                switch (c)
+                {
+                    case '(': opened.Push(')'); break;
+                    case '[': opened.Push(']'); break;
+                    case '{': opened.Push('}'); break;
+                    case '<': opened.Push('>'); break;
+                    case ')':
+                    case ']':
+                    case '}':
+                    case '>':
+                        char expected = opened.Pop();
+                        if (c != expected) return GetCorruptedScore(c);
+                        break;
+                    default: throw new ArgumentException("Invalid character");
+                }
+            }
+            return 0;
+        }
+
+        private int GetCorruptedScore(char c)
+        {
+            switch (c)
+            {
+                case ')': return 3;
+                case ']': return 57;
+                case '}': return 1197;
+                case '>': return 25137;
+            }
+            throw new ArgumentException("Invalid character");
+        }
+
+        public string Solve2(string input)
+        {
+            var file = ParseInput(input);
+            return GetIncompleteScore(file).ToString();
+        }
+
+        private long GetIncompleteScore(string[] file)
+        {
+            var scores = file.Select(GetIncompleteScore).Where(x => x != 0);
+            return scores.OrderBy(x => x).ElementAt(scores.Count() / 2);
+        }
+
+        private long GetIncompleteScore(string x)
+        {
+            Stack<char> opened = new Stack<char>();
+            for (int i = 0; i < x.Length; i++)
+            {
+                char c = x[i];
+                switch (c)
+                {
+                    case '(': opened.Push(')'); break;
+                    case '[': opened.Push(']'); break;
+                    case '{': opened.Push('}'); break;
+                    case '<': opened.Push('>'); break;
+                    case ')':
+                    case ']':
+                    case '}':
+                    case '>':
+                        char expected = opened.Pop();
+                        if (c != expected) return 0;
+                        break;
+                    default: throw new ArgumentException("Invalid character");
+                }
+            }
+            long sum = 0;
+            foreach (var item in opened)
+            {
+                sum = sum * 5 + GetIncompleteScore(item);
+            }
+            return sum;
+        }
+
+        private int GetIncompleteScore(char c)
+        {
+            switch (c)
+            {
+                case ')': return 1;
+                case ']': return 2;
+                case '}': return 3;
+                case '>': return 4;
+            }
+            throw new ArgumentException("Invalid character");
+        }
+
+        private string[] ParseInput(string input)
+        {
+            return input.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+        }
+    }
+}
---
+++ AocNetTest/Day10Test.cs
@@ -0,0 +1,71 @@
+﻿using AocNetLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AocNetTest
+{
+    [TestClass]
+    public class Day10Test
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            string input = @"[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]";
+            var solver = new Day10();
+            string output = solver.Solve(input);
+            Assert.AreEqual("26397", output);
+        }
+
+        [TestMethod]
+        public void Final1()
+        {
+            string input = File.ReadAllText("../../../files/day10.txt");
+            var solver = new Day10();
+            string output = solver.Solve(input);
+            Console.WriteLine(output);
+            Assert.AreEqual("374061", output);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            string input = @"[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]";
+            var solver = new Day10();
+            string output = solver.Solve2(input);
+            Assert.AreEqual("288957", output);
+        }
+
+        [TestMethod]
+        public void Final2()
+        {
+            string input = File.ReadAllText("../../../files/day10.txt");
+            var solver = new Day10();
+            string output = solver.Solve2(input);
+            Console.WriteLine(output);
+            Assert.AreEqual("2116639949", output);
+        }
+    }
+}
