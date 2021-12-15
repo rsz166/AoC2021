2cb72a51b97e90df0db04d5d7978095c343ce92e
rsz166 <szabolcs.roman6@gmail.com>
2021-12-15 22:59:27 +0100

    day15

---
+++ AocNetLib/Day15.cs
@@ -0,0 +1,110 @@
+﻿using System.Text;
+
+namespace AocNetLib
+{
+    public class Day15
+    {
+        public string Solve(string input, int mapSize = 1)
+        {
+            var map = ParseInput(input, mapSize);
+            File.WriteAllText("debug.txt", map.ToString());
+            map.FillCosts();
+            return map.Last.ToString();
+        }
+
+        private Map ParseInput(string input, int mapSize)
+        {
+            var lines = input.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+            int w = lines[0].Length;
+            int h = lines.Length;
+            var map = new Map(w * mapSize, h * mapSize);
+            for (int x1 = 0; x1 < mapSize; x1++)
+            {
+                for (int y1 = 0; y1 < mapSize; y1++)
+                {
+                    for (int x = 0; x < w; x++)
+                    {
+                        for (int y = 0; y < h; y++)
+                        {
+                            int v = (lines[y][x] - '0' + x1 + y1 - 1) % 9 + 1;
+                            map.Table[x + w * x1, y + h * y1] = v;
+                        }
+                    }
+                }
+            }
+            return map;
+        }
+
+        class Map
+        {
+            int width, height;
+
+            public readonly int[,] Table;
+            public readonly int[,] Costs;
+
+            public int Last => Costs[width - 1, height - 1];
+
+            public Map(int w, int h)
+            {
+                width = w; height = h;
+                Table = new int[w, h];
+                Costs = new int[w, h];
+            }
+
+            public void FillCosts()
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        Costs[x, y] = int.MaxValue;
+                    }
+                }
+                Costs[0, 0] = 0;
+                bool isUpdated;
+                do
+                {
+                    isUpdated = false;
+                    for (int x = 0; x < width; x++)
+                    {
+                        for (int y = 0; y < height; y++)
+                        {
+                            int cost = FindMin(x, y);
+                            if (cost < Costs[x, y])
+                            {
+                                Costs[x, y] = cost;
+                                isUpdated = true;
+                            }
+                        }
+                    }
+                }
+                while (isUpdated);
+            }
+
+            int FindMin(int x, int y)
+            {
+                int cost = Table[x, y];
+                int min = Costs[x, y] - cost;
+                if (x > 0 && min > Costs[x - 1, y]) min = Costs[x - 1, y];
+                if (y > 0 && min > Costs[x, y - 1]) min = Costs[x, y - 1];
+                if (x < width - 2 && min > Costs[x + 1, y]) min = Costs[x + 1, y];
+                if (y < height - 2 && min > Costs[x, y + 1]) min = Costs[x, y + 1];
+                return min + cost;
+            }
+
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        sb.Append(Table[x, y]);
+                    }
+                    sb.AppendLine();
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ AocNetTest/Day15Test.cs
@@ -0,0 +1,75 @@
+﻿using AocNetLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AocNetTest
+{
+    [TestClass]
+    public class Day15Test
+    {
+        private const string FilePath = "../../../files/day15.txt";
+
+        Day15 GetSolver() => new Day15();
+
+        [TestMethod]
+        public void Test1()
+        {
+            string input = @"1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581";
+            var solver = GetSolver();
+            string output = solver.Solve(input);
+            Assert.AreEqual("40", output);
+        }
+
+        [TestMethod]
+        public void Final1()
+        {
+            string input = File.ReadAllText(FilePath);
+            var solver = GetSolver();
+            string output = solver.Solve(input);
+            Console.WriteLine(output);
+            Assert.AreEqual("717", output);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            string input = @"1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581";
+            var solver = GetSolver();
+            string output = solver.Solve(input, 5);
+            Assert.AreEqual("315", output);
+        }
+
+        [TestMethod]
+        public void Final2()
+        {
+            string input = File.ReadAllText(FilePath);
+            var solver = GetSolver();
+            string output = solver.Solve(input, 5);
+            Console.WriteLine(output);
+            Assert.AreEqual("2993", output);
+        }
+    }
+}
