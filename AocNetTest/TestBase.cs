948445969f917a7b669fa8362642a1200a0e6c09
rsz166 <szabolcs.roman6@gmail.com>
2021-12-18 23:54:35 +0100

    day18/1

---
+++ AocNetTest/TestBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AocNetTest
+{
+    public abstract class TestBase<T> where T : new()
+    {
+        protected string FilePath => string.Format("../../../files/{0}.txt", typeof(T).Name.ToLower());
+
+        protected T GetSolver() => new();
+
+        protected string GetFinalInput() => File.ReadAllText(FilePath);
+    }
+}
