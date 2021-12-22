9ab62b3e2c1a7f2039b1b2b52c8f1ff5a2c76199
rsz166 <szabolcs.roman6@gmail.com>
2021-12-22 11:38:08 +0100

    day21

--- AocNetTest/Day21Test.cs
+++ AocNetTest/Day21Test.cs
@@ -31,17 +31,25 @@ Player 2 starting position: 8"));
         [TestMethod]
         public void Test2()
         {
-            //var solver = GetSolver();
-//            Assert.AreEqual("444356092776315", solver.Solve2(@"Player 1 starting position: 4
-//Player 2 starting position: 8"));
+            var solver = GetSolver();
+            Assert.AreEqual("444356092776315", solver.Solve2(@"Player 1 starting position: 4
+Player 2 starting position: 8"));
         }
 
+
+        [TestMethod]
+        public void Test2_1()
+        {
+            var solver = GetSolver();
+            Assert.AreEqual("", solver.Solve2(@"Player 1 starting position: 2
+Player 2 starting position: 1"));
+        }
         [TestMethod]
         public void Final2()
         {
-            //var solver = GetSolver();
-            //string output = solver.Solve2(GetFinalInput());
-            //Console.WriteLine(output);
+            var solver = GetSolver();
+            string output = solver.Solve2(GetFinalInput());
+            Console.WriteLine(output);
             //Assert.AreEqual("4647", output);
         }
     }
