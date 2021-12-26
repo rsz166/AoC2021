3982816cbcc306f6083b93a86cb9f6e499ca6b01
rsz166 <szabolcs.roman6@gmail.com>
2021-12-26 23:19:22 +0100

    day25

---
+++ AocNetLib/Day25.cs
@@ -0,0 +1,99 @@
+﻿namespace AocNetLib
+{
+    public class Day25
+    {
+        public string Solve(string v)
+        {
+            var map = ParseInput(v);
+            //Console.WriteLine(map);
+            int cnt = 0;
+            bool isMoved;
+            do
+            {
+                isMoved = false;
+                if (map.Move('>')) isMoved = true;
+                if (map.Move('v')) isMoved = true;
+                cnt++;
+                //Console.WriteLine(cnt);
+                //Console.WriteLine(map);
+            } while (isMoved);
+            return cnt.ToString();
+        }
+
+        Map ParseInput(string input)
+        {
+            return new Map(input.TrimEnd().Split(new char[] { '\n' }).Select(x => x.Trim().ToCharArray()).ToArray());
+        }
+
+        class Map
+        {
+            int Width, Height;
+            char[][] map;
+            char[][] cache;
+            public Map(char[][] map)
+            {
+                this.map = map;
+                Height = map.Length;
+                Width = map[0].Length;
+                cache = new char[Height][];
+                for (int i = 0; i < Height; i++)
+                {
+                    cache[i] = new char[Width];
+                }
+            }
+
+            void CopyCache()
+            {
+                for (int r = 0; r < Height; r++)
+                {
+                    for (int c = 0; c < Width; c++)
+                    {
+                        cache[r][c] = map[r][c];
+                    }
+                }
+            }
+
+            public bool Move(char target)
+            {
+                CopyCache();
+                bool moved = false;
+                for (int r = 0; r < Height; r++)
+                {
+                    for (int c = 0; c < Width; c++)
+                    {
+                        if (map[r][c] == target)
+                        {
+                            if(target == '>')
+                            {
+                                if (map[r][(c + 1)%Width] == '.')
+                                {
+                                    cache[r][(c + 1) % Width] = target;
+                                    cache[r][c] = '.';
+                                    moved = true;
+                                }
+                            }
+                            else
+                            {
+                                if (map[(r+1)%Height][c] == '.')
+                                {
+                                    cache[(r + 1) % Height][c] = target;
+                                    cache[r][c] = '.';
+                                    moved = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                var tmp = map;
+                map = cache;
+                cache = tmp;
+                return moved;
+            }
+
+            public override string ToString()
+            {
+                return string.Join(Environment.NewLine, map.Select(x=>string.Concat(x)));
+            }
+        }
+    }
+}
---
+++ AocNetTest/Day25Test.cs
@@ -0,0 +1,60 @@
+﻿using AocNetLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AocNetTest
+{
+    [TestClass]
+    public class Day25Test : TestBase<Day25>
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var solver = GetSolver();
+            Assert.AreEqual("58", solver.Solve(@"v...>>.vv>
+.vv>>.vv..
+>>.>v>...v
+>>v>>.>.v.
+v>v.vv.v..
+>.>>..v...
+.vv..>.>v.
+v.v..>>v.v
+....v..v.>"));
+        }
+
+        [TestMethod]
+        public void Final1()
+        {
+            var solver = GetSolver();
+            string output = solver.Solve(GetFinalInput());
+            Console.WriteLine(output);
+            //Assert.AreEqual("5259", output);
+        }
+
+        //        [TestMethod]
+        //        public void Test2()
+        //        {
+        //            var solver = GetSolver();
+        //            Assert.AreEqual("3351", solver.Solve(@"..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+        //#..#.
+        //#....
+        //##..#
+        //..#..
+        //..###", 50));
+        //        }
+
+        //        [TestMethod]
+        //        public void Final2()
+        //        {
+        //            var solver = GetSolver();
+        //            string output = solver.Solve(GetFinalInput(), 50);
+        //            Console.WriteLine(output);
+        //            Assert.AreEqual("15287", output);
+        //        }
+    }
+}
